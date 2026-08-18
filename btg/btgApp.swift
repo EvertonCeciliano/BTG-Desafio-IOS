60ace6819ee431ba3db001bdffa6839d57e70070
everton <evertoncecilianosouza@gmail.com>
2026-08-18 20:10:39 -0300

    Initial Commit

---
+++ btg/btgApp.swift
@@ -0,0 +1,17 @@
+//
+//  btgApp.swift
+//  btg
+//
+//  Created by Everton Ceciliano on 18/08/26.
+//
+
+import SwiftUI
+
+@main
+struct btgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
